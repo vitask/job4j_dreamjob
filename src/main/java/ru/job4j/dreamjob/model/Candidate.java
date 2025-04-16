840c4e8e43adbb7803366b0877ef0d9a6b0bb329
vitask <skalenko.v@yandex.ru>
2025-04-16 12:23:51 +0700

    5. Список кандидатов [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -0,0 +1,68 @@
+package ru.job4j.dreamjob.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Candidate {
+
+    private int id;
+    private String name;
+    private String Description;
+    private LocalDateTime createDate;
+
+    public Candidate(int id, String name, String description, LocalDateTime createDate) {
+        this.id = id;
+        this.name = name;
+        Description = description;
+        this.createDate = createDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Candidate candidate = (Candidate) o;
+        return id == candidate.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -0,0 +1,60 @@
+package ru.job4j.dreamjob.repository;
+
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryCandidateRepository implements CandidateRepository {
+
+    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Candidate> candidates = new HashMap<>();
+
+    private MemoryCandidateRepository() {
+        save(new Candidate(0, "Oleg Ivanov", "Intern Java developer", LocalDateTime.now()));
+        save(new Candidate(0, "Semen Borisov", "Junior Java developer", LocalDateTime.now()));
+        save(new Candidate(0, "Maria Petrova", "Junior+ Java developer", LocalDateTime.now()));
+        save(new Candidate(0, "Denis Aliev", "Middle Java developer", LocalDateTime.now()));
+        save(new Candidate(0, "Tanya Stepanova", "Middle+ Java developer", LocalDateTime.now()));
+        save(new Candidate(0, "Max Zhuravlev", "Senior Java developer", LocalDateTime.now()));
+
+    }
+
+    public static MemoryCandidateRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Candidate save(Candidate candidate) {
+        candidate.setId(nextId++);
+        candidates.put(candidate.getId(), candidate);
+        return candidate;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        candidates.remove(id);
+    }
+
+    @Override
+    public boolean update(Candidate candidate) {
+        return candidates.computeIfPresent(candidate.getId(),
+                (id, oldCandidate) -> new Candidate(oldCandidate.getId(), candidate.getName(), candidate.getDescription(), candidate.getCreateDate())) != null;
+    }
+
+    @Override
+    public Optional<Candidate> findById(int id) {
+        return Optional.ofNullable(candidates.get(id));
+    }
+
+    @Override
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
