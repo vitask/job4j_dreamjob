9e552b96668204ae0b938bc9992e0663cc8bf7c4
vitask <skalenko.v@yandex.ru>
2025-04-21 17:15:22 +0700

    8. HTML form. Страница создания вакансии [#504844]

--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -8,7 +8,7 @@ import ru.job4j.dreamjob.repository.MemoryVacancyRepository;
 import ru.job4j.dreamjob.repository.VacancyRepository;
 
 @Controller
-@RequestMapping("/vacancies") /* Работать с кандидатами будем по URI /vacancies/** */
+@RequestMapping("/vacancies")
 public class VacancyController {
 
     private final VacancyRepository vacancyRepository = MemoryVacancyRepository.getInstance();
@@ -17,5 +17,10 @@ public class VacancyController {
     public String getAll(Model model) {
         model.addAttribute("vacancies", vacancyRepository.findAll());
         return "vacancies/list";
+    }
+
+    @GetMapping("create")
+    public String getCreationPage() {
+        return "vacancies/create";
     }
 }
