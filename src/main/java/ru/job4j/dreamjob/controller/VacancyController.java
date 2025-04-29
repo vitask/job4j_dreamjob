8380995f07e66191f31e637b502ece96bbed9076
vitask <skalenko.v@yandex.ru>
2025-04-29 12:22:40 +0700

    4. Создание вакансии [#504849]

--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -1,9 +1,13 @@
 package ru.job4j.dreamjob.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.dreamjob.model.Vacancy;
 import ru.job4j.dreamjob.repository.MemoryVacancyRepository;
 import ru.job4j.dreamjob.repository.VacancyRepository;
 
@@ -22,5 +26,11 @@ public class VacancyController {
     @GetMapping("create")
     public String getCreationPage() {
         return "vacancies/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Vacancy vacancy) {
+        vacancyRepository.save(vacancy);
+        return "redirect:/vacancies";
     }
 }
--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -8,7 +8,10 @@ public class Vacancy {
     private int id;
     private String title;
     private String description;
-    private LocalDateTime creationDate;
+    private LocalDateTime creationDate = LocalDateTime.now();
+
+    public Vacancy() {
+    }
 
     public Vacancy(int id, String title, String description) {
         this.id = id;
